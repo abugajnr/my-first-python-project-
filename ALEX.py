a1e1e75baba977cb5ba7b75ea1c57973fe5b23d0
abugajnr <alexabuga04@gmail.com>
2026-03-22 14:52:48 +0100

    Add files via upload

---
+++ ALEX.py
@@ -0,0 +1,21 @@
+import random
+
+print("hello Alex")
+name = "Alex"
+print ("Alex")
+a = 9
+b = 12
+print ( a + b)
+numbers = 3,2,2,4,8,9,1,9,2,5
+print (random.choice(numbers))
+number = random.randint (1,10)
+guess = int(input(75))
+if guess == number:
+    print("you win")
+else:
+    print("try again")
+
+
+
+
+
